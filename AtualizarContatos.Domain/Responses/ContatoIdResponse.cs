606baa3b28b7dab6c25a345aed5c1c663de4ae55
matmiranda <mat.miranda@outlook.com>
2025-03-22 00:48:36 -0300

    ajustes

--- AtualizarContatos.Domain/Responses/ContatoIdResponse.cs
+++ AtualizarContatos.Domain/Responses/ContatoIdResponse.cs
@@ -3,10 +3,10 @@
     public class ContatoIdResponse
     {
         public int Id { get; set; }
-        public string Nome { get; set; }
-        public string Telefone { get; set; }
-        public string Email { get; set; }
+        public string? Nome { get; set; }
+        public string? Telefone { get; set; }
+        public string? Email { get; set; }
         public int DDD { get; set; }
-        public string Regiao { get; set; }
+        public string? Regiao { get; set; }
     }
 }
