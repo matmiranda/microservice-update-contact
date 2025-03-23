2c55fedce63808c49f6c2d4f2493540db2abba40
matmiranda <mat.miranda@outlook.com>
2025-03-22 22:33:46 -0300

    ajustes

--- AtualizarContatos.Domain/Responses/ContatoIdResponse.cs
+++ AtualizarContatos.Domain/Responses/ContatoIdResponse.cs
@@ -1,12 +1,20 @@
-﻿namespace AtualizarContatos.Domain.Responses
+﻿using System.Text.Json.Serialization;
+
+namespace AtualizarContatos.Domain.Responses
 {
     public class ContatoIdResponse
     {
+        [JsonPropertyName("id")]
         public int Id { get; set; }
+        [JsonPropertyName("nome")]
         public string? Nome { get; set; }
+        [JsonPropertyName("Telefone")]
         public string? Telefone { get; set; }
+        [JsonPropertyName("email")]
         public string? Email { get; set; }
+        [JsonPropertyName("ddd")]
         public int DDD { get; set; }
+        [JsonPropertyName("regiao")]
         public string? Regiao { get; set; }
     }
 }
